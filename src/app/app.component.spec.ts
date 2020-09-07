a57889a03d84c5da7f3604ce6642f36e1d96ded8
gayaprasad <dinesh01ga@gmail.com>
2020-09-07 19:36:02 +0530

    linting issue solved

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,13 @@
 import { AppComponent } from './app.component';
 describe('AppComponent', () => {
-  let fixture : AppComponent;
-  beforeEach(()=>{
-    fixture = new AppComponent()
-  })
-  
-   
+  let fixture: AppComponent;
+  beforeEach(() => {
+    fixture = new AppComponent();
+  });
   it('should create the app', () => {
     expect(fixture).toBeTruthy();
   });
-    
   it('should create the app', () => {
     expect(fixture.title).toEqual('blogs');
   });
-  });
+});
